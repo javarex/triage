adb776c59f7b5a2870092e3323e65eca036bc16a
javarex <raymartnayreitanong11@gmail.com>
2020-10-08 09:37:46 +0800

    downloadable qrCode

--- resources/views/triage/index.blade.php
+++ resources/views/triage/index.blade.php
@@ -135,41 +135,6 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="profile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Profile</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body" id="printProfile">
-        <div class="row container">
-            <div class="col-md-12 px-0 d-flex justify-content-center">
-                <span style="cursor:pointer" title="Scan me!">{!! QrCode::size('200')->generate(Auth::user()->username) !!}</span>
-               
-            </div>
-            <div class="col-md-12 px-0 d-flex justify-content-center">
-                <h1>{{ Auth::user()->first_name.' '.Auth::user()->last_name}}</h1>>
-            </div>
-            <div class="col-md-12 px-0 d-flex justify-content-center">
-                <label for=""><i class="fas fa-mobile-alt    "></i> Contact #: {{ $client_id->contact_number }}</label>
-            </div>
-            <div class="col-md-12 px-0 d-flex justify-content-center">
-                <label for=""><i class="fas fa-map-marker-alt    "></i> Address: {{ $client_id->address }}</label>
-            </div>
-           
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <a href="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(300)->generate( Auth::user()->username )) !!}" class="btn btn-primary" id="print_qr" download="triage_QRCode"><i class="fa fa-fw fa-save" aria-hidden="true"></i>Save</a>
-       
-      </div>
-    </div>
-  </div>
-</div>
 
 
 @endsection
