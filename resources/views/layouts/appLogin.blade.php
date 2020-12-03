619a1fcf23c1a5f7b6e219f95d3b1cc3f5b12802
javarex <raymartnayreitanong11@gmail.com>
2020-12-03 11:25:37 +0800

    121212

---
+++ resources/views/layouts/appLogin.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>DdO QR</title>
+    <!-- Fonts -->
+    
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet"> 
+   
+    <link href="{{ asset('css/pageLoader.css') }}" rel="stylesheet">
+
+
+    @yield('styles')
+</head>
+
+<body style="background-image: radial-gradient(#fff3c0 , #fcd538, gold)">
+    <!-- for page loader -->
+    <div class="o-page-loader">
+        <div class="o-page-loader--content">
+            <div class="o-page-loader--spinner"></div>
+            <div class="o-page-loader--message">
+                <span>Loading...</span>
+            </div>
+        </div>
+    </div>
+
+    @if(auth()->user())
+        @if(auth()->user()->role == 0)
+            @include('layouts.navbar.navbar_admin')
+        @elseif(auth()->user()->role == 1)
+            @include('layouts.navbar.navbar_establishment')
+        @endif
+    @endif
+
+    <main class="container py-3">
+        @yield('content')
+    </main>
+    
+
+
+    <script src="{{ asset('js/pageLoader.js') }}"></script>
+
+   
+    
+    @yield('scripts')
+</body>
+</html>
