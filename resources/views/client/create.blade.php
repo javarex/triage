01903507c48301aaba144936da0475588340820b
javarex <raymartnayreitanong11@gmail.com>
2021-01-13 00:29:45 +0800

    privacyNote51

--- resources/views/client/create.blade.php
+++ resources/views/client/create.blade.php
@@ -25,7 +25,7 @@
 <div class="">
     <div class="row justify-content-center ">
         <div class=" col-12 col-md-12 d-flex justify-content-center " >
-            <img src="{{ asset('image/triage_h.png') }}" class="pb-2" width="200" height="auto">
+            <img src="{{ asset('image/triage_h.png') }}" class="img-fluid pb-2" width="200" height="auto">
         </div>
 
         <div class="col-md-10">
