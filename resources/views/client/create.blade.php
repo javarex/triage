9e4cf0ff8eb2b4c316a9ba418a527d816a6bfc07
javarex <raymartnayreitanong11@gmail.com>
2021-01-13 00:24:46 +0800

    privacyNote5

--- resources/views/client/create.blade.php
+++ resources/views/client/create.blade.php
@@ -25,7 +25,7 @@
 <div class="">
     <div class="row justify-content-center ">
         <div class=" col-12 col-md-12 d-flex justify-content-center " >
-            <img src="{{ asset('image/triage_h.png') }}" class="img-fluid pb-2 d-block" width="200" style="max-width: 100%;height: auto;">
+            <img src="{{ asset('image/triage_h.png') }}" class="pb-2" width="200" height="200">
         </div>
 
         <div class="col-md-10">
