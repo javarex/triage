4dd8b667341654f8390a5a43c8d4188719315d76
javarex <raymartnayreitanong11@gmail.com>
2021-01-20 12:33:06 +0800

    citizen registration fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,47 +3,53 @@
 @section('content')
 <!-- end page loader -->
 <div class="container pt-5">
-		<div class="d-flex justify-content-center h-100">
-			<div class="user_card">
-				<div class="d-flex justify-content-center">
-					<div class="brand_logo_container">
-						<img src="{{ asset('image/triage1.png')}}" class="image-fluid" width="160" alt="Logo">
-					</div>
-				</div>
-				<div class="d-flex justify-content-center form_container">
-					<form action="{{route('login')}}" method="post" autocomplete="off">
-                    @csrf 
-						<div class="input-group mb-3">
-							<div class="input-group-append">
-								<span class="input-group-text"><i class="fas fa-user"></i></span>
-							</div>
-							<input type="text" name="username" class="form-control input_user @error('username') is-invalid @enderror" value="{{ old('username') }}" placeholder="username">
-						</div>
-						<div class="input-group mb-2">
-							<div class="input-group-append">
-								<span class="input-group-text"><i class="fas fa-key"></i></span>
-							</div>
-							<input type="password" name="password" class="form-control input_pass" placeholder="password">
-						</div>
-						
-							<div class="d-flex justify-content-center mt-3 login_container">
-				 	<button name="button" class="btn login_btn">Login</button>
-				   </div>
-					</form>
-				</div>
-		
-				<div class="mt-4">
-					<div class="d-flex justify-content-center links text-light">
-						Don't have an account? <a href="client/create" class="ml-2">Sign Up</a>
-					</div>
-					<div class="d-flex justify-content-center links text-light">
-						How to register? <a href="{{asset('files/citizenReg.pdf')}}" class="ml-2" target="_blank">Click here</a>
-					</div>
-				</div>
-                <div class="alert alert-info" role="alert">
-                    <strong>NOTE:</strong> Test run for capitol employees only.
+		<div class="row ">
+			<div class="col-md-12 d-flex justify-content-center">
+                <div class="user_card">
+                    <div class="d-flex justify-content-center">
+                        <div class="brand_logo_container">
+                            <img src="{{ asset('image/triage1.png')}}" class="image-fluid" width="160" alt="Logo">
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center form_container">
+                        <form action="{{route('login')}}" method="post" autocomplete="off">
+                                 @csrf 
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="text" name="username" class="form-control input_user @error('username') is-invalid @enderror" value="{{ old('username') }}" placeholder="username">
+                            </div>
+                            <div class="input-group mb-2">
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                </div>
+                                <input type="password" name="password" class="form-control input_pass" placeholder="password">
+                            </div>
+                            
+                                <div class="d-flex justify-content-center mt-3 login_container">
+                         <button name="button" class="btn login_btn">Login</button>
+                       </div>
+                        </form>
+                    </div>
+                
+                    <div class="mt-4">
+                        <div class="d-flex justify-content-center links text-light">
+                            Don't have an account? <a href="client/create" class="ml-2">Sign Up</a>
+                        </div>
+                        <div class="d-flex justify-content-center links text-light">
+                            How to register? <a href="{{asset('files/citizenReg.pdf')}}" class="ml-2" target="_blank">Click here</a>
+                        </div>
+                    </div>
                 </div>
-			</div>
+            </div>
+            <div class="col-md-12 d-flex justify-content-center">
+                <div class="col-md-4 p-0">
+                    <div class="alert alert-info" role="alert">
+                        <strong>NOTE:</strong> Test run for capitol employees only.
+                    </div>
+                </div>
+            </div>
 		</div>
 	</div>
 
